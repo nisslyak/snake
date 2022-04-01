b20a7596f47f863a42b007f3e533ff0c5a3803e9
nisslyak <nissopodolyak@gmail.com>
2022-04-01 15:12:32 +0600

    создали класс Point

--- snake/Point.cs
+++ snake/Point.cs
@@ -3,8 +3,14 @@ namespace snake
 {
     public class Point
     {
-        public Point()
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
         {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
--- snake/Program.cs
+++ snake/Program.cs
@@ -6,25 +6,22 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = char.Parse("*");
-            Draw(x1, y1, sym1);
+
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = char.Parse("*");
+            p1.Draw();
 
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = char.Parse("@");
-            Draw(x2, y2, sym2);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym=char.Parse("@");
+            p2.Draw();
 
             Console.ReadLine();
 
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
